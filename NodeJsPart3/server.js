5a3a45d52c5861e42b83b17622955b6aa2e0ebbf
Erika Mediratta <erikamediratta@gmail.com>
2026-01-19 22:36:29 +0530

    server.js changes made

--- NodeJsPart3/server.js
+++ NodeJsPart3/server.js
@@ -14,11 +14,27 @@ const PORT= process.env.PORT||3500;
 
 //creating an http server
 
-const server=http.createServer((req,res)=>{
-        console.log(req.url,req.method);
+const server = http.createServer((req, res) => {
+        
+    console.log(req.url, req.method);
 
+    let filePath;  
+    if (req.url === '/' || req.url === '/index.html') {
+        res.statusCode = 200;   
+        res.setHeader("Content-Type", "text/html");
+
+        filePath = path.join(__dirname, "views", "index.html");
+
+        fs.readFile(filePath, "utf8", (err, data) => {
+            if (err) {
+                res.statusCode = 500;
+                res.end("Server Error");
+            } else {
+                res.end(data);
+            }
+        });
+    }
 });
-
 
 //start server- 
 server.listen(PORT,()=>console.log("Server running on Port", PORT));
